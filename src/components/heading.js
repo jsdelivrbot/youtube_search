fb2509e85cde4a7338f8275eabe687fe44b855fd
mattdavida <mattdavida@outlook.com>
2017-05-01 14:30:56 -0400

    Edit heading component and change css

--- src/components/heading.js
+++ src/components/heading.js
@@ -2,8 +2,8 @@ import React from "react";
 
 const Heading = () => {
   return (
-    <div className="jumbotron">
-      <h1>My take on youtube search:</h1>
+    <div className="jumbotron text-center">
+      <h1>YourTube</h1>
       <h2>Search through any of YouTube&#39;s videos.  made using ReactJS</h2>
     </div>
   )
